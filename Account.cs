78449b14a8dd65dd8c89d9ddfd49f217d9dfb392
Brendon W. <bwherry@midland.k12.ia.us>
2026-02-19 11:55:54 -0600

    Update Account.cs

--- Account.cs
+++ Account.cs
@@ -4,10 +4,11 @@ public class Account {
 
    public static string Name = "Dave";
 
-   public static string ChangeName (string ) 
+   public static string ChangeName (string newName) 
    {
        Name = "Jack";
       
        return Name;
    }  
 }
+
