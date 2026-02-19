3fc09e8edc9506701c184736e9b321f955b8eebb
Brendon W. <bwherry@midland.k12.ia.us>
2026-02-19 12:17:52 -0600

    Update Account.cs

--- Account.cs
+++ Account.cs
@@ -1,14 +1,21 @@
 ﻿namespace knightmoves;
 
-public class Account {
+public class Account
+{
+    // Public static class-level string variable
+    public static string Name;
 
-   static string Name = "Dave";
+    // Static constructor
+    static Account()
+    {
+        Name = "Main Account";
+    }
 
-   static string ChangeName () 
-   {
-       Name = "Jack";
-      
-       return Name;
-   }  
+    // Public static method that takes a parameter
+    public static string ChangeName(string newName)
+    {
+        Name = newName;
+        return Name;
+    }
 }
 
